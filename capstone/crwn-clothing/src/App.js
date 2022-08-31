8abf7a66805e1a70a6101c99d6f1ad6b39da7c9f
crist1230 <mateas.cri@gmail.com>
2022-08-30 23:47:33 -0700

    added routes

--- capstone/crwn-clothing/src/App.js
+++ capstone/crwn-clothing/src/App.js
@@ -1,9 +1,24 @@
-import Directory from './components/directory/directory.component';
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+
+const Shop = () => {
+  return (
+    <div>
+      I am the shop!
+    </div>
+  )
+};
 
 const App = () => {
-
   return (
-    <Directory/>
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
+    </Routes>
   );
 };
 
