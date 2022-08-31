69ba1cfaf82e634f1e1060df3a3e8758110558f3
crist1230 <mateas.cri@gmail.com>
2022-08-31 15:34:54 -0700

    add a sign in component and notes to routes

--- capstone/crwn-clothing/src/App.js
+++ capstone/crwn-clothing/src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/sign-in/sign-in.component';
 
 const Shop = () => {
   return (
@@ -13,11 +14,16 @@ const Shop = () => {
 
 const App = () => {
   return (
-    <Routes>
-      <Route path='/' element={<Navigation />}>
-        <Route index element={<Home />} />
+    <Routes> {/* a wrapper that contains all the routes that our website will point to */}
+      <Route path='/' element={<Navigation />}> 
+        <Route index element={<Home />} /> {/* index says when the route matches the parent route render this element too*/}
         <Route path='shop' element={<Shop />} />
+        <Route path='signin' element={<SignIn />} />
       </Route>
+      {/* 
+        if someone visits '/' then the nav & home will be shown, but if someone visits '/shop'
+        then keep the nav element but update the home element to be the shop element
+      */}
     </Routes>
   );
 };
