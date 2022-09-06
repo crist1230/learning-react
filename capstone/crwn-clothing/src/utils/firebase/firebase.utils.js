4cb664c42bf943654cdcb474a2263bd66f8c515d
Cristiana <webscr.fo@gmail.com>
2022-09-06 13:40:11 -0700

    added styling and authentication and sign-in component

--- capstone/crwn-clothing/src/utils/firebase/firebase.utils.js
+++ capstone/crwn-clothing/src/utils/firebase/firebase.utils.js
@@ -5,7 +5,9 @@ import {
     signInWithRedirect, 
     signInWithPopup, 
     GoogleAuthProvider, 
-    createUserWithEmailAndPassword } from 'firebase/auth';
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword
+} from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
 // my specific online firebase
@@ -85,3 +87,8 @@ export const createAuthUserWithEmailAndPassword = async (email, password) => {
     if (!email || !password) return;
     return await createUserWithEmailAndPassword(auth, email, password);
 };
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+    if (!email || !password) return;
+    return await signInWithEmailAndPassword(auth, email, password);
+}
