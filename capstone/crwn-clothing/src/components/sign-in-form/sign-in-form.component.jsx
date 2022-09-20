acc3a4d7999c7dd806e27d7e5a0ba858551434b7
crist1230 <mateas.cri@gmail.com>
2022-09-19 17:53:16 -0700

    more merging

--- capstone/crwn-clothing/src/components/sign-in-form/sign-in-form.component.jsx
+++ capstone/crwn-clothing/src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,107 +1,12 @@
-<<<<<<< HEAD
-import { useState, useContext } from 'react';
-
-import FormInput from '../form-input/form-input.component';
-import Button from '../button/button.component';
-
-import { UserContext } from '../../contexts/user.context'; // will give back whatever value was passed in for the value (in the user.context.jsx)
-
-import {
-  signInWithGooglePopup,
-  createUserDocumentFromAuth,
-  signInAuthUserWithEmailAndPassword,
-=======
 import { useState } from "react";
 
 import {
     signInWithGooglePopup,
     signInAuthUserWithEmailAndPassword
->>>>>>> relearning-firebase
 } from '../../utils/firebase/firebase.utils';
 
 import './sign-in-form.styles.scss';
 
-<<<<<<< HEAD
-const defaultFormFields = {
-  email: '',
-  password: '',
-};
-
-const SignInForm = () => {
-  const [formFields, setFormFields] = useState(defaultFormFields);
-  const { email, password } = formFields;
-
-  const { setCurrentUser } = useContext(UserContext);
-
-  const resetFormFields = () => {
-    setFormFields(defaultFormFields);
-  };
-
-  const signInWithGoogle = async () => {
-    const { user } = await signInWithGooglePopup();
-    await createUserDocumentFromAuth(user);
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    try {
-      const {user} = await signInAuthUserWithEmailAndPassword(email, password);
-      setCurrentUser(user);
-
-      resetFormFields();
-    } catch (error) {
-      switch (error.code) {
-        case 'auth/wrong-password':
-          alert('incorrect password for email');
-          break;
-        case 'auth/user-not-found':
-          alert('no user associated with this email');
-          break;
-        default:
-          console.log(error);
-      }
-    }
-  };
-
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-
-    setFormFields({ ...formFields, [name]: value });
-  };
-
-  return (
-    <div className='sign-up-container'>
-      <h2>Already have an account?</h2>
-      <span>Sign in with your email and password</span>
-      <form onSubmit={handleSubmit}>
-        <FormInput
-          label='Email'
-          type='email'
-          required
-          onChange={handleChange}
-          name='email'
-          value={email}
-        />
-
-        <FormInput
-          label='Password'
-          type='password'
-          required
-          onChange={handleChange}
-          name='password'
-          value={password}
-        />
-        <div className='buttons-container'>
-          <Button type='submit'>Sign In</Button>
-          <Button type='button' buttonType='google' onClick={signInWithGoogle}>
-            Google sign in
-          </Button>
-        </div>
-      </form>
-    </div>
-  );
-=======
 import Button from "../button/button.component";
 import FormInput from "../form-input/form-input.component";
 
@@ -179,7 +84,6 @@ const SignInForm = () => {
             </form>
         </div>
     );
->>>>>>> relearning-firebase
 };
 
 export default SignInForm;
