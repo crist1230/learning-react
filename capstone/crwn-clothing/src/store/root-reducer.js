4bf121cc50d400327f2d5e56ff6e61268d892afc
Cristiana <webscr.fo@gmail.com>
2022-10-05 09:10:37 -0700

    lecture 163

--- capstone/crwn-clothing/src/store/root-reducer.js
+++ capstone/crwn-clothing/src/store/root-reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 
 import { userReducer } from './user/user.reducer';
+import { cartReducer } from './cart/cart.reducer';
 
 // this is the state of the reducer
 export const rootReducer = combineReducers({
-  user: userReducer
+  user: userReducer,
+  cart: cartReducer
 });
