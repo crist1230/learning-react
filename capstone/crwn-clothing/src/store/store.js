37db0e427b19caf0824e71ac893817a4aefc2f04
webscrfo <99758865+webscrfo@users.noreply.github.com>
2022-10-10 13:48:43 -0700

    finished module 15

--- capstone/crwn-clothing/src/store/store.js
+++ capstone/crwn-clothing/src/store/store.js
@@ -1,12 +1,32 @@
 import { compose, legacy_createStore as createStore, applyMiddleware } from 'redux';
+
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
 
-// middleware is what the dispatched action will hit before going to the reducers
-const middleWares = [logger];
+const persistConfig = {
+  key: 'root', // where you want the persisting to reach (so here the root of the app)
+  storage, // where you want to store persisted value (here this means local storage)
+  blacklist: ['user'] // values we don't want persisted (because user is being listened to we don't need to persist)
+};
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// middleware is what the dispatched action will hit before going to the reducers
+const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
+
+const composedEnhancer = 
+  (process.env.NODE_ENV !== 'production' &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhancers = composedEnhancer(applyMiddleware(...middleWares));
 
 // every time the state changes, it will hit this line of code and the change will be logged
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(persistedReducer, undefined, composedEnhancers);
+
+export const persistor = persistStore(store);
