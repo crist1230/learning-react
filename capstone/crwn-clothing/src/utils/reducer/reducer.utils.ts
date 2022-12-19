4b1c018016a1fdf44fadb79735b990d6d9008c2a
Cristiana <mateas.cri@gmail.com>
2022-12-19 09:39:19 -0800

    working on 198

--- capstone/crwn-clothing/src/utils/reducer/reducer.utils.ts
+++ capstone/crwn-clothing/src/utils/reducer/reducer.utils.ts
@@ -9,7 +9,7 @@ export type Action<T> = {
   type: T;
 };
 
-export function createAction<T extends string, P>(type: T, payload: P): {type: T, payload: P } {
+export function createAction<T extends string, P>(type: T, payload: P): {type: T, payload: P} {
   return { type, payload };
 };
 
