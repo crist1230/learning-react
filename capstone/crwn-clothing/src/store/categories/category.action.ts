413b5ee13faabd42000a285480edbc8dbc6b6d05
Cristiana <mateas.cri@gmail.com>
2022-12-27 15:18:02 -0800

    lec 202

--- capstone/crwn-clothing/src/store/categories/category.action.ts
+++ capstone/crwn-clothing/src/store/categories/category.action.ts
@@ -6,10 +6,10 @@ export const fetchCategoriesStart = () => (
   createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START)
 );
 
-export const fetchCategoriesSuccess = (categoriesArray) => (
+export const fetchCategoriesSuccess = (categoriesArray: object[]) => (
   createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, categoriesArray)
 );
 
-export const fetchCategoriesFailed = (error) => (
+export const fetchCategoriesFailed = (error: Error) => (
   createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error) 
 );
