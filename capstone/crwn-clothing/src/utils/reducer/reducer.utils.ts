f834ab78f4fdc362abc169485f0b63dfcb6d8303
Cristiana <mateas.cri@gmail.com>
2022-12-27 13:09:05 -0800

    lecture 198 actions

--- capstone/crwn-clothing/src/utils/reducer/reducer.utils.ts
+++ capstone/crwn-clothing/src/utils/reducer/reducer.utils.ts
@@ -1,16 +1,24 @@
 import { AnyAction } from 'redux';
 
+// this type is an object with key values type and payload
 export type ActionWithPayload<T, P> = {
   type: T;
   payload: P;
 };
 
+// this type is an object with key value type
 export type Action<T> = {
   type: T;
 };
 
-export function createAction<T extends string, P>(type: T, payload: P): {type: T, payload: P} {
+// * Overload Signature
+export function createAction<T extends string, P>(type: T, payload: P): ActionWithPayload<T, P>;
+
+// * Overload Signature
+export function createAction<T extends string>(type: T, payload: void): Action<T>;  
+
+// * Implementation Signature
+// // export const createAction = (type, payload) => ({ type, payload });
+export function createAction<T extends string, P>(type: T, payload: P) {
   return { type, payload };
 };
-
-// export const createAction = (type, payload) => ({ type, payload });
